fd447d407f630577be7f6f805b8665604c91e3c6
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-09-18 17:43:22 -0400

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const contentDiv = document.getElementById('content')
+const title = document.createElement('h1');
+
+title.textContent = "To-Do List";
+
+contentDiv.appendChild(title);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, './src/index.js')
+
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
