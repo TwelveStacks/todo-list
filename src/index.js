cd0cfc89338d402f8a551202118ea6e01748606d
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-09-18 22:25:25 -0400

    Added css loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './style.css';
+
 const contentDiv = document.getElementById('content')
 const title = document.createElement('h1');
 
 title.textContent = "To-Do List";
+title.classList.add('title');
 
 contentDiv.appendChild(title);
