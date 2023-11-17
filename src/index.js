840c873a6ee91d876a07b83d582c3e784a819da7
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-11-17 00:25:55 -0500

    Changed css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style.css';
 
-const header = document.getElementById('header');
-const contentDiv = document.getElementById('content');
-const title = document.createElement('h1');
+// const header = document.getElementById('header');
+// const contentDiv = document.getElementById('content');
+// const title = document.createElement('h1');
 
-title.textContent = "To-Do List";
-title.classList.add('title');
+// header.getElementsByTagName
 
-contentDiv.appendChild(title);
+// title.textContent = "To-Do List";
+// title.classList.add('title');
+
+// header.appendChild(title);
