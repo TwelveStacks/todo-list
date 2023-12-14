ba92d1ae501ed39c84b7be817e9b29783f15fb23
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-12-14 14:47:49 -0500

    Add event listener to 'New' button to create a new project.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './style.css';
 
+const newProject = document.querySelector('#new-project');
+
+newProject.addEventListener("click", () => {
+    
+});
+
+class Project {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+function createProject(Project) {
+    
+}
+
 // const header = document.getElementById('header');
 // const contentDiv = document.getElementById('content');
 // const title = document.createElement('h1');
